1e9d7c96a7eda1b1a707999e8c7f5e53a06898d7
liyongakang <liyongkang@163.com>
2017-05-02 15:41:44 +0800

    修改

--- test/src/main/java/com/bonc/lyk/constant/BaseConstant.java
+++
@@ -1,8 +0,0 @@
-package com.bonc.lyk.constant;
-
-public class BaseConstant {
-
-	public static final String STATUS_OK = "200";
-	public static final String STATUS_ERROR = "500";
-	public static final String STATUS_VALIDATEERROR = "401";
-}
